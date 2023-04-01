9fe62cde968ed606a32ff8475f21c61e7e2549c6
Collin MacDonald <cdm.helloworld@gmail.com>
2023-04-01 01:01:27 -0400

    refactoring

--- server/src/courselist_wrapper.py
+++ server/src/courselist_wrapper.py
@@ -16,29 +16,42 @@ def update_courselist_db():
         'TERM_END_DATE': '2023-12-31T00:00:00'
     })
 
-def test_courselist_db():
+    # Clear Terms
 
-	print('test_courselist_db')
+    # Update Terms
 
-	# db = firestore.client()  # this connects to our Firestore database
-    # collection = db.collection('terms')  # opens 'places' collection
-    
-    # docs = collection.get()
+    # Clear Subjects
 
-    # for doc_raw in docs:
-    #     doc = doc_raw.to_dict()
-
-    #     results = f'{doc["TERM_CODE"]} {doc["TERM_DESC"]} {doc["TERM_END_DATE"]}'
+    # Update Subjects
 
 
-    # creation_result = collection.document('TEST-TERM-2').set({
-    #     'TERM_CODE': '202410',
-    #     'TERM_DESC': 'Fall 2023',
-    #     'TERM_END_DATE': '2023-12-31T00:00:00'
-    # })
 
-    # context = { 
-    #     'server_time': server_time,
-    #     'results': results,
-    #     'creation_result': creation_result
-    # }
+def get_all_terms():
+
+	db = firestore.client()
+    collection = db.collection('terms')
+
+    terms_docs = collection.get()
+
+    terms_dicts = []
+
+    for term_doc in terms_docs:
+        term_dict = term_doc.to_dict()
+        terms_dicts.append(term_dict)
+
+	return terms_dicts
+
+def get_all_subjects():
+
+	db = firestore.client()
+    collection = db.collection('subjects')
+
+    subjects_docs = collection.get()
+
+    subjects_dicts = []
+
+    for subject_doc in subjects_dicts:
+        subject_dict = subject_doc.to_dict()
+        subjects_dicts.append(subject_dict)
+
+	return subjects_dicts
