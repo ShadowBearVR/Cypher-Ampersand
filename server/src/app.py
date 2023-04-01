0819560cf785277c7ad4611a7d1195c5d291f02c
Collin MacDonald <cdm.helloworld@gmail.com>
2023-03-31 22:57:26 -0400

    basic database interactions

--- server/src/app.py
+++ server/src/app.py
@@ -3,7 +3,18 @@ import os
 import time
 from datetime import datetime
 
+import firebase_admin
+from firebase_admin import credentials, firestore
+
 app = Flask(__name__)
+
+cred = credentials.Certificate("env/cypher-ampersand-firebase-adminsdk-2694w-0d791d1fab.json")
+firebase_admin.initialize_app(cred)
+
+
+
+
+
 
 def format_server_time():
 	server_time = time.localtime()
@@ -15,7 +26,30 @@ def index():
 
 	server_time = format_server_time()
 
-	context = { 'server_time': server_time }
+
+	db = firestore.client()  # this connects to our Firestore database
+	collection = db.collection('terms')  # opens 'places' collection
+	
+	docs = collection.get()
+
+	for doc_raw in docs:
+		doc = doc_raw.to_dict()
+
+		results = f'{doc["TERM_CODE"]} {doc["TERM_DESC"]} {doc["TERM_END_DATE"]}'
+
+
+	creation_result = collection.document('TEST-TERM-2').set({
+	    'TERM_CODE': '202410',
+	    'TERM_DESC': 'Fall 2023',
+	    'TERM_END_DATE': '2023-12-31T00:00:00'
+	})
+
+	context = { 
+		'server_time': server_time,
+		'results': results
+		'creation_result': creation_result
+	}
+
 	return render_template('index.html', context=context)
 
 @app.errorhandler(403)
